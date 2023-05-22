ef9df82da6afe281557db41c1d62805664d4c109
iHoerth <hoerth.ignacio@gmail.com>
2023-05-21 22:20:06 -0300

    deploy no funca

---
+++ src/helpers/urlVariables.js
@@ -0,0 +1,18 @@
+// export const URL_PATIENTS = `http://localhost:3001/patients`;
+// export const URL_DOCTORS = `http://localhost:3001/doctors`;
+// export const URL_SPECIALTIES = `http://localhost:3001/specialties`;
+// export const URL_SOCIALSECURITY = `http://localhost:3001/socialSecurity`;
+// export const URL_PERFILMEDICO = `http://localhost:3001/perfilMedico`;
+// export const URL_MAIL = `http://localhost:3001/mail`;
+// export const URL_POSTAGENDA = `http://localhost:3001/horarios/bulk`;
+// export const URL_TURNOS = `http://localhost:3001/turno`;
+// export const URL_PERFILPACIENTE = `http://localhost:3001/perfilPaciente`;
+
+export const URL_PATIENTS = `http://apideploy-production.up.railway.app/patients`;
+export const URL_DOCTORS = `http://apideploy-production.up.railway.app/doctors`;
+export const URL_SPECIALTIES = `http://apideploy-production.up.railway.app/specialties`;
+export const URL_SOCIALSECURITY = `http://apideploy-production.up.railway.app/socialSecurity`;
+export const URL_PERFILMEDICO = `http://apideploy-production.up.railway.app/perfilMedico`;
+export const URL_MAIL = `http://apideploy-production.up.railway.app/mail`;
+export const URL_POSTAGENDA = `http://apideploy-production.up.railway.app/horarios/bulk`;
+export const URL_TURNOS = `http://apideploy-production.up.railway.app/turno`;
